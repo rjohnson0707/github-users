bbc2e3f0bb56645c7ac3ed4bfd8085be1b94f27f
Will Johnson <will@PRO.local>
2020-02-25 16:05:10 -0700

    Final Push

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
+const rootUrl = 'https://api.github.com/'
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const userName = req.query.username;
-  console.log(`username: ${username}`);
-  res.render('index');
+  const token = process.env.GITHUB_TOKEN;
+  const options = {
+    url: `${rootUrl}users/${userName}`,
+    headers: {
+      'User-Agent': 'rjohnson0707',
+      Authorization: `token ${token}`
+    }
+  };
+  request(options, function(err, response, body) {
+    const userData = JSON.parse(body);
+    options.url = userData.repos_url;
+    request(options, function(err, response, body) {
+      userData.repos = JSON.parse(body);
+      console.log(userData.repos[0]);
+      res.render('index', {userData});
+    })
+  });
 });
 
 module.exports = router;
